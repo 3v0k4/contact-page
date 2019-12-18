5b6c19ac1e230b2d0b4e02a34ddda088c9db2342
3v0k4 <riccardo.odone@gmail.com>
2019-12-18 23:53:08 +0100

    scaffold post script

---
+++ blog/scaffold.hs
@@ -0,0 +1,33 @@
+#!/usr/bin/env stack
+{- stack
+  script
+  --resolver lts-14.17
+  --package directory
+  --package time
+-}
+
+import System.Directory
+import Control.Monad
+import Data.Foldable
+import Data.Time.Clock
+import Data.Time.Format
+
+frontmatter :: String
+frontmatter = unlines
+  [ "---"
+  , "title:"
+  , "description:"
+  , "author:"
+  , "cover_image:"
+  , "tags:"
+  , "  - FunctionalProgramming"
+  , "---"
+  ]
+
+main :: IO ()
+main = do
+  today <- formatTime defaultTimeLocale "%F" <$> getCurrentTime
+  let fileName = fold ["posts", "/", today, "-"]
+  fileExist <- doesFileExist fileName
+  when fileExist $ error "file already exists"
+  writeFile fileName frontmatter
