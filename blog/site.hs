539d89253879e903f3350e187885e4ac1f72a165
3v0k4 <riccardo.odone@gmail.com>
2020-05-28 18:50:58 +0200

    tweetable pull quotes

--- blog/site.hs
+++ blog/site.hs
@@ -8,18 +8,26 @@ import Data.Binary (Binary)
 import Data.Bool (bool)
 import Data.Foldable (fold, traverse_)
 import Data.List (group, nub, partition, sortOn)
-import Data.Maybe (fromMaybe)
+import Data.Maybe (fromJust, fromMaybe)
+import Data.Text (pack, replace, unpack)
 import Data.Traversable (traverse)
 import Data.Typeable (Typeable)
 import Hakyll
+import Network.HTTP.Base (urlEncode)
 import System.Environment (getEnvironment)
 import System.FilePath (replaceExtension)
 import Text.Blaze.Html ((!), toHtml, toValue)
 import Text.Blaze.Html.Renderer.String (renderHtml)
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
+import Text.Pandoc.Definition (Block (..), Inline (..))
+import Text.Pandoc.Walk (walkM)
 
 --------------------------------------------------------------------------------
+
+root :: String
+root = "https://odone.io"
+
 feedConfiguration :: FeedConfiguration
 feedConfiguration =
   FeedConfiguration
@@ -27,7 +35,7 @@ feedConfiguration =
       feedDescription = "Rambling on software as a learning tool",
       feedAuthorName = "Riccardo Odone",
       feedAuthorEmail = "",
-      feedRoot = "https://odone.io"
+      feedRoot = root
     }
 
 previewHost' :: String
@@ -64,7 +72,7 @@ main = do
       let livePath = (`replaceExtension` "html") . toFilePath
       route . customRoute $ livePath
       compile $
-        pandocCompiler
+        pandocCompiler'
           >>= loadAndApplyTemplate "templates/post.html" (postCtx tags)
           >>= saveSnapshot "content"
           >>= loadAndApplyTemplate "templates/default.html" (postCtx tags)
@@ -216,3 +224,34 @@ getTags' identifier = do
   metadata <- getMetadata identifier
   pure $ fromMaybe ["_untagged_"] $
     lookupStringList "tags" metadata <|> (map trim . splitAll "," <$> lookupString "tags" metadata)
+
+pandocCompiler' :: Compiler (Item String)
+pandocCompiler' = pandocCompilerWithTransformM defaultHakyllReaderOptions defaultHakyllWriterOptions (walkM transform)
+  where
+    transform :: Block -> Compiler Block
+    transform (CodeBlock (_, ["pullquote"], []) content) = do
+      identifier <- getUnderlying
+      meta <- getMetadata identifier
+      let toTwitterTag = ("#" <>) . unpack . replace " " "" . pack
+      let tags = unwords . fmap toTwitterTag . fromMaybe [] . lookupStringList "tags" $ meta
+      maybeRoute <- getRoute identifier
+      let url = ((root <> "/") <>) . fromJust $ maybeRoute
+      let text = urlEncode . fold $ [unpack content, " ", "via @RiccardoOdone", "\n\n", tags, "\n\n", url]
+      let query = fold ["text", "=", text]
+      let href = pack $ "https://twitter.com/intent/tweet?" <> query
+      let twitterIcon = "<svg class=\"tweet-this-icon\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 400 400\"><defs><style>.cls-1{fill:none;}.cls-2{fill:#f76ca5;}</style></defs><title>Twitter_Logo_Blue</title><rect class=\"cls-1\" width=\"400\" height =\"400\"/><path class=\"cls-2\" d=\"M153.62,301.59c94.34,0,145.94-78.16,145.94-145.94,0-2.22,0-4.43-.15-6.63A104.36,104.36,0,0,0,325,122.47a102.38,102.38,0,0,1-29.46,8.07,51.47,51.47,0,0,0,22.55-28.37,102.79,102.79,0,0,1-32.57,12.45,51.34,51.34,0,0,0-87.41,46.78A145.62,145.62,0,0,1,92.4,107.81a51.33,51.33,0,0,0,15.88,68.47A50.91,50.91,0,0,1,85,169.86c0,.21,0,.43,0,.65a51.31,51.31,0,0,0,41.15,50.28,51.21,51.21,0,0,1-23.16.88,51.35,51.35,0,0,0,47.92,35.62,102.92,102.92,0,0,1-63.7,22A104.41,104.41,0,0,1,75,278.55a145.21,145.21,0,0,0,78.62,23\"/></svg>"
+      pure
+        $ RawBlock "html"
+        $ fold
+          [ "<blockquote class=\"pullquote\">",
+            "<span>",
+            content,
+            "</span>",
+            "<a target=\"_blank\" rel=\"noopener\" href=",
+            href,
+            ">",
+            twitterIcon,
+            "</a>",
+            "</blockquote>"
+          ]
+    transform x = pure x
