c29f06a71a585f3992a84ce3ef62411c10c6cd1c
3v0k4 <riccardo.odone@gmail.com>
2022-04-29 10:58:52 +0200

    fix: some assets were not being deployed

--- deploy.sh
+++ deploy.sh
@@ -52,7 +52,7 @@ cd -
 cd typescript.tips
 npm install
 npx tailwindcss -i input.css -o dist/output.css --minify
-cp index.html dist/
+cp -f public/* dist/
 sed -i '' -e "s/VERSION/$(date +%s)/g" dist/index.html
 rsync \
   --rsync-path="sudo rsync" \
--- typescript.tips/tailwind.config.js
+++ typescript.tips/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./index.html"],
+  content: ["public/index.html"],
   theme: {
     extend: {
       colors: {
