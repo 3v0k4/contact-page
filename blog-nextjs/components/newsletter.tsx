898d9e5d4ee29b8a09e5be4c3ab75ffab8e5f368
3v0k4 <riccardo.odone@gmail.com>
2022-07-18 23:26:47 +0200

    blog: left aligned newsletter bullets

--- blog-nextjs/components/newsletter.tsx
+++ blog-nextjs/components/newsletter.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 
 const Newsletter = () => (
-  <div id="newsletter" className="newsletter-container text-center py-5 mt-5">
+  <div id="newsletter" className="newsletter-container tw-text-center py-5 mt-5">
     <div className="col-12 col-sm-12 col-md-6 col-xl-6 mx-auto mt-sm-50">
       <h2>PinkLetter</h2>
 
@@ -16,7 +16,7 @@ const Newsletter = () => (
         <input className="input-lg form-control fs-bigger" type="email" name="email" id="bd-email" placeholder="Email address" required />
         <input type="hidden" value="1" name="embed" />
 
-        <div className="mt-4 text-left">
+        <div className="mt-4 tw-text-left">
           <input className="btn btn-primary fs-bigger w-100 white-space-normal" type="submit" value="Send me evergreen software wisdom!"></input>
         </div>
       </form>
@@ -30,7 +30,7 @@ const Newsletter = () => (
       <h2>Tired of RELEARNING webdev stuff?</h2>
 
       <div className="mt-25 w-80 mx-auto">
-        <ul className="text-left list-style-circle list-style-inside p-0 m-0">
+        <ul className="tw-text-left list-style-circle list-style-inside p-0 m-0">
           <li>A 79-page book with the best links I curated over the years</li>
           <li className="mt-5-">An email once a week full of timeless software wisdom</li>
           <li className="mt-5-">Your recommended weekly dose of pink</li>
