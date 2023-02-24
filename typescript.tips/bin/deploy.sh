20c59978b58bec0e6202ee92aec09f4306f377ff
3v0k4 <riccardo.odone@gmail.com>
2023-02-24 09:16:50 +0100

    tips(infra): just deploy to aws

--- typescript.tips/bin/deploy.sh
+++ typescript.tips/bin/deploy.sh
@@ -9,10 +9,6 @@ npx ts-node bin/sitemap.ts
 cp -rf public/* dist/
 npx tailwindcss -i src/input.css -o dist/output.css --minify
 npx ts-node bin/build.ts
-rsync \
-  --rsync-path="sudo rsync" \
-  -a \
-  dist/ riccardo@odone.io:/usr/share/nginx/html/typescript.tips/
 aws s3 sync ./dist s3://typescript.tips
 aws cloudfront create-invalidation \
     --distribution-id E2K2FTV79X2003 \
