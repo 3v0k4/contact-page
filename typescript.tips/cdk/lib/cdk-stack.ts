b3fba256198f50185522eaf54b1df9a2909f9f9c
3v0k4 <riccardo.odone@gmail.com>
2023-02-28 20:28:14 +0100

    tips: use route53
    Namecheap's ALIAS on the APEX was doing something strange and making Cloudfront go to LAX or SF instead of WAW, while the www CNAME was going to WAW. Moved to Route53 so that both the APEX and www get to WAW.

--- typescript.tips/cdk/lib/cdk-stack.ts
+++ typescript.tips/cdk/lib/cdk-stack.ts
@@ -4,6 +4,8 @@ import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as route53 from "aws-cdk-lib/aws-route53";
+import * as route53Targets from "aws-cdk-lib/aws-route53-targets";
 import { Construct } from "constructs";
 
 const DOMAIN_NAME = "typescript.tips";
@@ -19,13 +21,21 @@ export class CdkStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.RETAIN,
     });
 
+    const hostedZone = new route53.PublicHostedZone(
+      this,
+      `${DOMAIN_NAME}-route53-hosted-zone`,
+      {
+        zoneName: DOMAIN_NAME,
+      }
+    );
+
     const certificate = new acm.Certificate(
       this,
       `${DOMAIN_NAME}-acm-certificate`,
       {
         domainName: DOMAIN_NAME,
         subjectAlternativeNames: [`www.${DOMAIN_NAME}`],
-        validation: acm.CertificateValidation.fromDns(),
+        validation: acm.CertificateValidation.fromDns(hostedZone),
       }
     );
 
@@ -59,6 +69,22 @@ export class CdkStack extends cdk.Stack {
         },
       }
     );
+
+    new route53.ARecord(this, `${DOMAIN_NAME}-route53-a-record`, {
+      zone: hostedZone,
+      recordName: DOMAIN_NAME,
+      target: route53.RecordTarget.fromAlias(
+        new route53Targets.CloudFrontTarget(distribution)
+      ),
+    });
+
+    new route53.ARecord(this, `${DOMAIN_NAME}-route53-www-a-record`, {
+      zone: hostedZone,
+      recordName: `www.${DOMAIN_NAME}`,
+      target: route53.RecordTarget.fromAlias(
+        new route53Targets.CloudFrontTarget(distribution)
+      ),
+    });
 
     new s3deploy.BucketDeployment(this, `${DOMAIN_NAME}-s3-deploy`, {
       sources: [s3deploy.Source.asset(ASSETS_PATH)],
